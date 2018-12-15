03e3848f709e26a5a79191ec960223f6f2056bd9
Jayanth Kanugo <imcopypasta@Jayanths-MacBook-Pro.local>
2018-12-15 14:46:56 +0530

    Updates

--- src/pages/resume.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { withPrefix } from 'gatsby'
-
-const ResumePage = () => <img src={withPrefix('/img/logo.png')} alt="Logo" />
-
-export default ResumePage
--- src/utils/typography.js
+++
@@ -1,6 +0,0 @@
-import Typography from 'typography'
-import FunstonTheme from 'typography-theme-funston'
-
-const typography = new Typography(FunstonTheme)
-
-export default typography
